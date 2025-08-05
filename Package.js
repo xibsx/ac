80a7c458415aa30349245a6791081cdf8812bc9c
xibsx <xibstech@gmail.com>
2025-08-05 16:00:27 +0300

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,16 @@
+{
+  "name": "sagittarius-terminator",
+  "version": "1.0.0",
+  "description": "WhatsApp Bot Sagittarius Terminator and Pairing Site in one project.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.7.2",
+    "express": "^4.19.2",
+    "pino": "^9.2.0"
+  },
+  "author": "Sagittarius Terminator",
+  "license": "ISC"
+}
